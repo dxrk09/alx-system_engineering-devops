2f797d09aac59880fb3e23e3465fdba29f73cee0
Dxrk <118543407+dxrk09@users.noreply.github.com>
2024-03-26 03:51:41 +0000

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-package { 'flask':
-    ensure   => '2.1.0',  # Ensure version 2.1.0 is installed
-    provider => 'pip3',   # Use pip3 as the package provider
-  }
-
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
